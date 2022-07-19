2a188eaa8d505d292389ecdeadc458d9ab230e67
Kuoi <starsareintherose@outlook.com>
2022-07-19 20:58:50 +0100

    add sum check

--- man_img.sh
+++ man_img.sh
@@ -7,4 +7,6 @@ git pull
 ./gen_img.sh
 
 rm -rf /usr/share/lilac/Repo/wsl/*
-mv bioarchlinux-$(date "+%Y%m%d").tar /usr/share/lilac/Repo/wsl/
+md5sum bioarchlinux-$(date "+%Y%m%d").tar > bioarchlinux-$(date "+%Y%m%d").tar.md5
+sha1sum bioarchlinux-$(date "+%Y%m%d").tar > bioarchlinux-$(date "+%Y%m%d").tar.sha1
+mv bioarchlinux-$(date "+%Y%m%d").tar* /usr/share/lilac/Repo/wsl/
