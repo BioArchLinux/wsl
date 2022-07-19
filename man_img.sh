a6cd7e751e2e71648bdbdb99525e41154be069dd
Kuoi <starsareintherose@outlook.com>
2022-07-19 21:02:15 +0100

    Update man_img.sh

--- man_img.sh
+++ man_img.sh
@@ -9,4 +9,5 @@ git pull
 rm -rf /usr/share/lilac/Repo/wsl/*
 md5sum bioarchlinux-$(date "+%Y%m%d").tar > bioarchlinux-$(date "+%Y%m%d").tar.md5
 sha1sum bioarchlinux-$(date "+%Y%m%d").tar > bioarchlinux-$(date "+%Y%m%d").tar.sha1
-mv bioarchlinux-$(date "+%Y%m%d").tar* /usr/share/lilac/Repo/wsl/
+mv bioarchlinux-$(date "+%Y%m%d").tar /usr/share/lilac/Repo/wsl/
+mv bioarchlinux-$(date "+%Y%m%d").tar.* /usr/share/lilac/Repo/wsl/
