eb285349e95ab399b38be7830b86a3fbbdda17d2
Kuoi <starsareintherose@outlook.com>
2022-07-21 02:31:29 +0100

    make all sums

--- man_img.sh
+++ man_img.sh
@@ -7,7 +7,10 @@ git pull
 ./gen_img.sh
 
 rm -rf /usr/share/lilac/Repo/wsl/*
-md5sum bioarchlinux-$(date "+%Y%m%d").tar > bioarchlinux-$(date "+%Y%m%d").tar.md5
-sha1sum bioarchlinux-$(date "+%Y%m%d").tar > bioarchlinux-$(date "+%Y%m%d").tar.sha1
+
+for bin in $(pacman -Ql coreutils | grep 'sum$' | sed 's/\// /g' | awk '{print $4}')
+do
+	$bin bioarchlinux-$(date "+%Y%m%d").tar > bioarchlinux-$(date "+%Y%m%d").tar.$bin
+	mv bioarchlinux-$(date "+%Y%m%d").tar.$bin /usr/share/lilac/Repo/wsl
+done
 mv bioarchlinux-$(date "+%Y%m%d").tar /usr/share/lilac/Repo/wsl/
-mv bioarchlinux-$(date "+%Y%m%d").tar.* /usr/share/lilac/Repo/wsl/
