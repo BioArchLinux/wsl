102f9a88bb2b32c0d0ce3934c4d0ebf5953e1b4d
BioArchLinuxBot <bioarchlinux@malacology.net>
2022-07-21 01:40:59 +0000

    change to bioarchlinux/bioarchlinux

--- gen_img.sh
+++ gen_img.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-docker build -t bioarchlinux:$(date "+%Y%m%d") .
-docker run -d bioarchlinux:$(date "+%Y%m%d")
+docker run -d bioarchlinux/bioarchlinux
 docker export $(docker ps -a | awk '{print $1}' |sed -n '2p') > bioarchlinux-$(date "+%Y%m%d").tar
 
 docker rm $(docker ps -aq)
